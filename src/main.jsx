6af23fbaad67febd9b6a71e1fc9901377ad6b3db
Victor <aue228lublupacanov@gmail.com>
2024-08-14 18:08:17 +0200

    photo path update

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import '/Users/admin/Desktop/ESN_Haaga-Helia/esnhaagahelia/src/index.css'
+import './index.css'
 import { BrowserRouter } from 'react-router-dom';
 
 
